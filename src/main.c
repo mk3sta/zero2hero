5a3db9e05a095c88f5a5ea8c78bc41bb0f2f5c84
root <root@DESKTOP-M8UI68L.localdomain>
2025-08-31 22:51:10 -0300

    .

--- src/main.c
+++ src/main.c
@@ -17,23 +17,23 @@ int main(int argc, char *argv[]){
     char *filepath= NULL;
     bool newfile = false;
     int c;
+    
     int dbfd = -1;
 
 
     while((c = getopt(argc, argv, "nf:")) != -1){
         switch (c){
-        case 'n':
-            newfile = true;
-            break;
-        case 'f':
-            filepath = optarg;
-            break;
-        case '?':
-            printf("Unknown option -%c\n",c);
-            break;
-        
-        default:
-            return -1;
+            case 'n':
+                newfile = true;
+                break;
+            case 'f':
+                filepath = optarg;
+                break;
+            case '?':
+                printf("Unknown option -%c\n",c);
+                break;            
+            default:
+                return -1;
         }
     }
 
@@ -44,8 +44,18 @@ int main(int argc, char *argv[]){
     }
 
     if (newfile){
+        printf("entre\n");
         dbfd = create_db_file(filepath);
-
+        if (dbfd == STATUS_ERROR){
+            printf("Unable to create database file\n");
+            return -1;
+        }else{
+            dbfd = open_db_file(filepath);
+             if (dbfd == STATUS_ERROR){
+                printf("Unable to open database file\n");
+                return -1;
+             }
+        }
     }
 
 
